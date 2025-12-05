c19569aa83e8477d9f013a4e815c30b8ab2bc279
julio573 <julio8497@hotmail.com>
2025-12-05 13:20:09 -0300

    Feat: add updateEmail endpoint

--- src/main/java/com/to_do/list/controller/UserController.java
+++ src/main/java/com/to_do/list/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.to_do.list.controller;
 
+import com.to_do.list.dto.UpdateEmailDTO;
 import com.to_do.list.dto.UserRequestDTO;
 import com.to_do.list.dto.UserResponseDTO;
-import com.to_do.list.entities.User;
 import com.to_do.list.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +26,11 @@ public class UserController {
     public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
         UserResponseDTO  userResponseDTO = userService.createUser(userRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDTO);
+    }
+
+    @PatchMapping("/{id}/email")
+    public ResponseEntity<UserResponseDTO> updateEmail(@PathVariable Long id, @Valid @RequestBody UpdateEmailDTO updateEmailDTO) {
+        UserResponseDTO userResponseDTO = userService.updateEmail(id, updateEmailDTO.getEmail());
+        return ResponseEntity.ok(userResponseDTO);
     }
 }
