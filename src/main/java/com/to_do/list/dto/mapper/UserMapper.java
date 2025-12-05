e7597074194fffa78956caecd76ad78c30578d87
julio573 <julio8497@hotmail.com>
2025-12-05 10:00:31 -0300

    Fix: Fixing wrong method

--- src/main/java/com/to_do/list/dto/mapper/UserMapper.java
+++ src/main/java/com/to_do/list/dto/mapper/UserMapper.java
@@ -1,17 +1,15 @@
 package com.to_do.list.dto.mapper;
 
 import com.to_do.list.dto.UserRequestDTO;
+import com.to_do.list.dto.UserResponseDTO;
 import com.to_do.list.entities.User;
-import jakarta.validation.constraints.Max;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
-    UserRequestDTO toDTO(User user);
+    UserResponseDTO toDTO(User user);
     User toEntity(UserRequestDTO dto);
 
 }
