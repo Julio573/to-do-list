d8beca8feccfd9c8a5bd3d6d94b744aaf610cfa8
julio573 <julio8497@hotmail.com>
2025-12-05 10:03:03 -0300

    Feat: Add UserService Register Method

---
+++ src/main/java/com/to_do/list/service/UserService.java
@@ -0,0 +1,26 @@
+package com.to_do.list.service;
+
+import com.to_do.list.dto.UserRequestDTO;
+import com.to_do.list.dto.UserResponseDTO;
+import com.to_do.list.dto.mapper.UserMapper;
+import com.to_do.list.entities.User;
+import com.to_do.list.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
+        User user = userMapper.toEntity(userRequestDTO);
+        user = userRepository.save(user);
+        return userMapper.toDTO(user);
+    }
+}
