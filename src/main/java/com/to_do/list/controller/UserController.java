ea8919ec4eac9c62a5aab55fd672e8af07dd799a
julio573 <julio8497@hotmail.com>
2025-12-05 10:03:37 -0300

    Feat: Add UserRegister Endpoint

---
+++ src/main/java/com/to_do/list/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.to_do.list.controller;
+
+import com.to_do.list.dto.UserRequestDTO;
+import com.to_do.list.dto.UserResponseDTO;
+import com.to_do.list.entities.User;
+import com.to_do.list.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/test")
+    public String checkStatus() {
+        return "Hello, World!";
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO  userResponseDTO = userService.createUser(userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDTO);
+    }
+}
