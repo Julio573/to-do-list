f071ba9491f17453dc06ed17325a6053a54c06eb
julio573 <julio8497@hotmail.com>
2025-12-05 13:18:26 -0300

    Feat: Add EmailDTO Class

---
+++ src/main/java/com/to_do/list/dto/UpdateEmailDTO.java
@@ -0,0 +1,19 @@
+package com.to_do.list.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateEmailDTO {
+
+    @Email
+    @NotBlank
+    private String email;
+}
