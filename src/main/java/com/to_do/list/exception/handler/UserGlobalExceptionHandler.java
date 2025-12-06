5f9084152440fb7dfe494c170731d427f3d150f5
julio573 <julio8497@hotmail.com>
2025-12-06 17:41:02 -0300

    Feat: GlobalExceptionHandler Class

---
+++ src/main/java/com/to_do/list/exception/handler/UserGlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.to_do.list.exception.handler;
+
+import com.to_do.list.dto.ApiErrorDTO;
+import com.to_do.list.exception.InvalidEmailException;
+import com.to_do.list.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class UserGlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiErrorDTO> handleUserNotFoundException(UserNotFoundException e) {
+        ApiErrorDTO apiErrorDTO = new ApiErrorDTO(
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiErrorDTO);
+    }
+
+
+}
