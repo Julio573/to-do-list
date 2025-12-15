6fcb233e1a48fb4c4d05978e0b07d83f72a56785
julio573 <julio8497@hotmail.com>
2025-12-15 18:45:35 -0300

    Feat: add UserServiceTest Class

---
+++ src/test/java/com/to_do/list/service/UserServiceTest.java
@@ -0,0 +1,29 @@
+package com.to_do.list.service;
+
+import com.to_do.list.dto.mapper.UserMapper;
+import com.to_do.list.repository.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserService userService;
+
+
+
+
+
+
+}
+
