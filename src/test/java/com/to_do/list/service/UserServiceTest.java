3e8f3c4e0480059fee2a2c9eac5536f5f6554a48
julio573 <julio8497@hotmail.com>
2025-12-16 11:00:47 -0300

    Test: Add unit testing method for user creation

--- src/test/java/com/to_do/list/service/UserServiceTest.java
+++ src/test/java/com/to_do/list/service/UserServiceTest.java
@@ -1,11 +1,23 @@
 package com.to_do.list.service;
 
+import com.to_do.list.dto.UserRequestDTO;
+import com.to_do.list.dto.UserResponseDTO;
 import com.to_do.list.dto.mapper.UserMapper;
+import com.to_do.list.entities.User;
 import com.to_do.list.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -21,7 +33,46 @@ public class UserServiceTest {
     private UserService userService;
 
 
+    @Nested
+    class SaveUserTests {
 
+        @Test
+        @DisplayName("Should create user successfully when everything is ok")
+        void shouldCreateUserSuccessfullyWhenEmailDoesNotExist() {
+            UserRequestDTO userRequestDTO = new UserRequestDTO();
+            userRequestDTO.setName("julio");
+            userRequestDTO.setEmail("julio@teste.com");
+            userRequestDTO.setPassword("Test@157");
+
+            User user = new User();
+            user.setName(userRequestDTO.getName());
+            user.setEmail(userRequestDTO.getEmail());
+            user.setPassword(userRequestDTO.getPassword());
+
+            when(userMapper.toEntity(userRequestDTO)).thenReturn(user);
+            when(userRepository.findByEmail(userRequestDTO.getEmail())).thenReturn(Optional.empty());
+            when(userRepository.save(user)).thenReturn(user);
+
+            UserResponseDTO userResponseDTO = new UserResponseDTO();
+            userResponseDTO.setName(userRequestDTO.getName());
+            userResponseDTO.setEmail(userRequestDTO.getEmail());
+
+            when(userMapper.toDTO(user)).thenReturn(userResponseDTO);
+
+            var result = userService.createUser(userRequestDTO);
+
+            assertThat(result).isNotNull();
+            assertThat(result.getName()).isEqualTo(userRequestDTO.getName());
+            assertThat(result.getEmail()).isEqualTo(userRequestDTO.getEmail());
+
+            verify(userMapper).toEntity(userRequestDTO);
+            verify(userRepository).findByEmail(userRequestDTO.getEmail());
+            verify(userRepository).save(user);
+            verify(userMapper).toDTO(user);
+
+
+        }
+    }
 
 
 
